8ee58f18b735df262ebdac79d1a5ce8e1ebc8427
glolorun <63762681+glolorun@users.noreply.github.com>
2020-12-08 04:18:05 -0500

    working on join component

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000
+const router = require('./router')
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('Welcome!');
+
+    socket.on('disconnect', () => {
+        console.log('User has left the chat')
+    })
+});
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
